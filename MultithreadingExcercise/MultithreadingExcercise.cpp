3d403efd57c0d1bf0f919a6dcc6b55dd0fd935b5
DartDaven <dartdaven@yandex.ru>
2023-07-31 21:35:00 +0400

    First working prototype

--- MultithreadingExcercise/MultithreadingExcercise.cpp
+++ MultithreadingExcercise/MultithreadingExcercise.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include <filesystem>
 #include <string>
+#include <vector>
+#include <thread>
+#include <fstream>
+#include <chrono>
 
 namespace fs = std::filesystem;
 
-int cointFilesInDirectory(const std::string& folderPath)
+void processFile(const std::string& fileName, int& sum)
 {
-    int fileCount{ 0 };
-    for (const auto& entry : fs::directory_iterator(folderPath))
-    {
-        if (fs::is_regular_file(entry)) { fileCount++; }
-    }
-    return fileCount;
+    std::ifstream file(fileName);
+    int number;
+    
+    //TO DO an additional check
+    file >> number;
+
+    //TO DO only filename without path
+    std::cout << fileName << ": " << number << std::endl;
+    sum += number;
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 int main(int argc, char* argv[])
@@ -40,10 +49,24 @@ int main(int argc, char* argv[])
         std::cout << "Default folder was chosen\n";
     }
 
-    int fileCount = cointFilesInDirectory(targetPath);
+    std::vector<std::thread> threads;
+    int totalSum{ 0 };
 
-    std::cout << "there is " << fileCount << " files\n";
+    for (const auto& entry : fs::directory_iterator(targetPath))
+    {
+        if (fs::is_regular_file(entry)) 
+        {
+            threads.emplace_back(processFile, entry.path().string(), std::ref(totalSum));
+        }
+    }
+
+    for (auto& thread : threads)
+    {
+        thread.join();
+    }
+
+    std::cout << "The sum is " << totalSum << std::endl;
+    
     std::cin.get();
-
     return 0;
 }
