d53a67d7da28ba6e78d7baf27f2d797c4d5ceaed
DartDaven <dartdaven@yandex.ru>
2023-07-31 20:12:45 +0400

    A program that counts files in a folder

--- MultithreadingExcercise/MultithreadingExcercise.cpp
+++ MultithreadingExcercise/MultithreadingExcercise.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <filesystem>
+#include <string>
 
-int main()
+namespace fs = std::filesystem;
+
+int cointFilesInDirectory(const std::string& folderPath)
 {
+    int fileCount{ 0 };
+    for (const auto& entry : fs::directory_iterator(folderPath))
+    {
+        if (fs::is_regular_file(entry)) { fileCount++; }
+    }
+    return fileCount;
+}
+
+int main(int argc, char* argv[])
+{
+    std::string targetPath;
+
+    if (argc >= 2)
+    {
+        targetPath = argv[1];
+    } 
+    else
+    {
+        std::cout << "Please enter the folder path\n";
+        std::getline(std::cin, targetPath);
+    }
+
+    if (targetPath.empty() || !fs::exists(targetPath)) 
+    { 
+        targetPath = "Test";
+        if (!fs::exists(targetPath))
+        {
+            std::cout << "Unfortunatelly there was no \"Test\" folder found nearby\n";
+            std::cin.get();
+            return 0;
+        }
+        std::cout << "Default folder was chosen\n";
+    }
+
+    int fileCount = cointFilesInDirectory(targetPath);
+
+    std::cout << "there is " << fileCount << " files\n";
+    std::cin.get();
+
     return 0;
 }
