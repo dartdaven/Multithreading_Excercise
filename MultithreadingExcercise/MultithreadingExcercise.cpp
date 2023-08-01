afac2d353bba408f65db1043498687b284309e29
DartDaven <dartdaven@yandex.ru>
2023-08-01 19:31:03 +0400

    Mutex added

--- MultithreadingExcercise/MultithreadingExcercise.cpp
+++ MultithreadingExcercise/MultithreadingExcercise.cpp
@@ -5,26 +5,54 @@
 #include <thread>
 #include <fstream>
 #include <chrono>
+#include <mutex>
 
 namespace fs = std::filesystem;
 
-void processFile(const std::string& fileName, int& sum)
-{
-    std::ifstream file(fileName);
-    int number;
-    
-    //TO DO an additional check
-    file >> number;
+std::mutex mtx;
 
-    //TO DO only filename without path
-    std::cout << fileName << ": " << number << std::endl;
+void processFile(const fs::path& filePath, int& sum)
+{
+    std::ifstream file(filePath);
+    
+    std::string fileContent;
+    int number{ 0 };
+    
+    //Check and print
+    if (std::getline(file, fileContent))
+    {
+        try 
+        {
+            number = std::stoi(fileContent);
+
+            std::lock_guard<std::mutex> lock(mtx);
+            std::cout << filePath.filename().string() << ": " << number << std::endl;
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "Wrong content in the file: " << filePath.string() << " with: " << e.what() << std::endl;
+            return;
+        }
+    }
+    else
+    {
+        std::cerr << "File " << filePath.string() << " is empty\n";
+        return;
+    }
+    
+    //modifing outer variable
+    std::unique_lock<std::mutex> lock(mtx);
+    
     sum += number;
+    
+    lock.unlock();
 
     std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 int main(int argc, char* argv[])
 {
+    //choose of the path
     std::string targetPath;
 
     if (argc >= 2)
@@ -42,13 +70,14 @@ int main(int argc, char* argv[])
         targetPath = "Test";
         if (!fs::exists(targetPath))
         {
-            std::cout << "Unfortunatelly there was no \"Test\" folder found nearby\n";
+            std::cerr << "Unfortunatelly there was no \"Test\" folder found nearby\n";
             std::cin.get();
             return 0;
         }
         std::cout << "Default folder was chosen\n";
     }
 
+    //multithreading part
     std::vector<std::thread> threads;
     int totalSum{ 0 };
 
@@ -56,7 +85,7 @@ int main(int argc, char* argv[])
     {
         if (fs::is_regular_file(entry)) 
         {
-            threads.emplace_back(processFile, entry.path().string(), std::ref(totalSum));
+            threads.emplace_back(processFile, entry.path(), std::ref(totalSum));
         }
     }
 
